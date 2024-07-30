94b917f69353ae84d9e235cb78bd9743fa4556b5
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-07-30 13:11:30 -0700

    task

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-const getStudentIdsSum = (students) => students.reduce((preVal, cur) => preVal + cur.id, 0);
-
-export default getStudentIdsSum;
+export default function getStudentIdsSum(students) {
+  return students.reduce((agg, student) => agg + student.id, 0);
+}
